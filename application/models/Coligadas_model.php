935cd18544307e516623ddf6624924b8fa9efdac
Anderson N. Isotton <anisotton@Hotmail.com>
2017-07-31 08:18:06 -0300

    Adição de parametrização e coligadas

---
+++ application/models/Coligadas_model.php
@@ -0,0 +1,18 @@
+<?php
+class Params_model extends SY_Model {
+
+    /**
+     * @var string
+     */
+    protected $table = 'smasy_coligada';
+
+    /**
+     * @var string
+     */
+    protected $colOrder = 'nome';
+
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'codigo';
+}
--- application/views/configuracoes/painel.php
+++
@@ -1,5 +0,0 @@
-<div class="row-fluid" style="margin-top: 0">
-    <div class="span12">
-        <strong>Painel Config</strong>
-    </div>
-</div>
